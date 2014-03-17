36d10fa2eb4c8858587f4cd7936bbedb74bf544b
a-salim <salim.abdullah@gmail.com>
2014-03-17 16:51:15 -0400

    fixed path

--- pkg/files-posix/scripts/monitor-amazon-ec2-instance-performance/monitor-amazon-ec2-instance-performance.sh
+++ pkg/files-posix/scripts/monitor-amazon-ec2-instance-performance/monitor-amazon-ec2-instance-performance.sh
@@ -2,4 +2,4 @@
 inst=`grep pidfile /etc/init.d/uptime_core | head -n 1 | cut -d: -f2 | rev | cut -c 12- | rev | sed -e 's/^[ \t]*//'`
 MIBDIRS=$inst/mibs
 export MIBDIRS
-python ../../plugins/scripts/monitor-amazon-ec2-instance-performance.py
+python ../../plugins/scripts/monitor-amazon-ec2-instance-performance/monitor-amazon-ec2-instance-performance.py
--- pkg/files-win/scripts/monitor-amazon-ec2-instance-performance/monitor-amazon-ec2-instance-performance.bat
+++ pkg/files-win/scripts/monitor-amazon-ec2-instance-performance/monitor-amazon-ec2-instance-performance.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-python ..\..\plugins\scripts\monitor-amazon-ec2-instance-performance.py
+python ..\..\plugins\scripts\monitor-amazon-ec2-instance-performance\monitor-amazon-ec2-instance-performance.py
